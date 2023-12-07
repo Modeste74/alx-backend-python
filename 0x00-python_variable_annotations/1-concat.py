ec4f5ba8b0f6d342528fcd103414aed99e3f4191
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-07 15:40:20 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""create a type-annotated
+function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function that takes 2 strings
+    and returns a concatenated string"""
+    return (str1 + str2)
