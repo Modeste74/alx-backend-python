71ce9b6041b960067b83f8b489cd4494d389dd7b
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-07 15:57:32 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""defines a type-annotated
+function to_str"""
+
+
+def to_str(n: float) -> str:
+    """takes in a float and return string
+    rep of the float"""
+    return str(n)
