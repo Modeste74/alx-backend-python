a1d41d410802bf48f8200a2eb49239c2d508caa5
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-07 22:20:09 +0300

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Annotating the function"""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Annotation done"""
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[int, int, int] = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
