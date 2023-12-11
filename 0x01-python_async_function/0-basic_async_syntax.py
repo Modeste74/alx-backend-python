cb38335d23b18f4b71da43e799aa9d4e0ba17c81
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-11 15:40:27 +0300

    The basics of async and annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,9 @@
 wait_random"""
 import asyncio
 import random
-from typing import Optional
 
 
-async def wait_random(max_delay: Optional[float] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay between
     0 and max_delay second and returns it"""
     delay = random.uniform(0, max_delay)
