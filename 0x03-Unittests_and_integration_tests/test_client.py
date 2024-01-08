d2d63e139a47fa411d7ceb0de861bb181a80bcd0
Modeste Ciira <ciiramodeste23@gmail.com>
2024-01-08 13:42:46 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -54,3 +54,13 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_pb_repos_url.assert_called_once()
         expected_repos = [repo['name'] for repo in payload]
         self.assertEqual(repos, expected_repos)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """test the has_license method"""
+        client = GithubOrgClient("example")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
